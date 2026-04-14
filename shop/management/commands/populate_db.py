4f0d434dee57e3dab67f60cc2bbbec37ed9c8752
Kuzver <verak7791@gmail.com>
2026-04-14 15:54:45 +0300

    Исправил populate_db для уникального slug у брендов

---
+++ shop/management/commands/populate_db.py
@@ -0,0 +1,49 @@
+from django.core.management.base import BaseCommand
+from faker import Faker
+import random
+from shop.models import Product, Category, Brand
+from django.utils.text import slugify
+
+fake = Faker('ru_RU')
+
+
+class Command(BaseCommand):
+    help = 'Заполняет базу тестовыми данными'
+
+    def handle(self, *args, **kwargs):
+        self.create_categories()
+        self.create_brands()
+        self.create_products(20)
+
+    def create_categories(self):
+        categories = ['Худи', 'Футболки', 'Обувь', 'Джинсы']
+
+        for name in categories:
+            Category.objects.get_or_create(
+                name=name,
+                slug=fake.unique.slug()
+            )
+
+    def create_brands(self):
+        for _ in range(5):
+            name = fake.company()
+            slug = fake.unique.slug()
+
+            Brand.objects.create(
+                name=name,
+                slug=slug
+            )
+
+    def create_products(self, count):
+        categories = Category.objects.all()
+        brands = Brand.objects.all()
+
+        for _ in range(count):
+            Product.objects.create(
+                name=fake.word(),
+                sku=fake.unique.ean(length=8),
+                price=random.randint(1000, 10000),
+                category=random.choice(categories),
+                brand=random.choice(brands),
+                description=fake.text()
+            )
